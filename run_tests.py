f687a0479a6c7df8d1295978fea834cd1f9fa4ef
mrbean-bremen <andreas.bohne@mevis.de>
2016-08-26 21:20:40 +0200

    Fixed run_tests.py after rename

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,6 @@ import sys
 import unittest
 
 if __name__ == '__main__':
-    test_suite = unittest.TestLoader().discover('tools')
+    test_suite = unittest.TestLoader().discover('dcm_spec_tools')
     result = unittest.TextTestRunner(verbosity=2).run(test_suite)
     sys.exit(0 if result.wasSuccessful() else 1)
